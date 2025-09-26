22322435b8a1e286e7c81d79f5332a580e29ec1d
Daniel Henninger <info@dhe.de>
2025-09-26 21:29:16 +0200

    Versionsnummer geändert

--- resources/views/livewire/pages/dashboard.blade.php
+++ resources/views/livewire/pages/dashboard.blade.php
@@ -1454,8 +1454,8 @@
                 <a href="https://www.passolution.de/agb/" target="_blank" rel="noopener noreferrer" class="hover:text-blue-300 transition-colors">AGB</a>
             </div>
             <div class="flex items-center space-x-4 text-sm">
-                <span>Version 1.0.14</span>
-                <span>Build: 2025-09-24</span>
+                <span>Version 1.0.15</span>
+                <span>Build: 2025-09-26</span>
             </div>
         </div>
     </footer>
